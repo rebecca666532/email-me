58481885c4ced5d906ccad791c49898490629037
杨松竹 <yangsongzhu@yangsonzhudembp.attlocal.net>
2020-04-12 17:14:47 -0400

    Vacation website

---
+++ Homework/Vacation website/js/vacation.js
@@ -0,0 +1,60 @@
+$(document).ready( function(){
+
+  $("#_css").click(function(){
+    $("header p").css("font-family: 'Shadows Into Light', cursive;");
+  });
+
+  $("footer").mouseover(function(){
+      $("footer p").html("Cuba is a beautiful country!");
+  });
+
+  $("footer").mouseleave(function() {
+    $("footer p").html("Hover over me!");
+  });
+
+  $("#set2").hide();
+  $("#set3").hide();
+  $("#set4").hide();
+  $("#set5").hide();
+
+  $("#button1").click(function(){
+    $("#set1").show();
+    $("#set2").hide();
+    $("#set3").hide();
+    $("#set4").hide();
+    $("#set5").hide();
+  });
+
+  $("#button2").click(function(){
+    $("#set1").hide();
+    $("#set2").show();
+    $("#set3").hide();
+    $("#set4").hide();
+    $("#set5").hide();
+  });
+
+  $("#button3").click(function(){
+    $("#set1").hide();
+    $("#set2").hide();
+    $("#set3").show();
+    $("#set4").hide();
+    $("#set5").hide();
+  });
+
+  $("#button4").click(function(){
+    $("#set1").hide();
+    $("#set2").hide();
+    $("#set3").hide();
+    $("#set4").show();
+    $("#set5").hide();
+  });
+
+  $("#button5").click(function(){
+    $("#set1").hide();
+    $("#set2").hide();
+    $("#set3").hide();
+    $("#set4").hide();
+    $("#set5").show();
+  });
+
+  });
